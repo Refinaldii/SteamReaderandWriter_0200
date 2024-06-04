28b6220b17165e5d17204ba3d9c7cfa579a4bcf0
Refinaldii <refinaldi9786@gmail.com>
2024-06-04 10:55:14 +0700

    membuat projek baru manual exeption

---
+++ Manualexeption/Manualexeption.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuuah integer maka
+		cout << "Pertnyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << " Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
+	return 0;
+}
+
+ 
